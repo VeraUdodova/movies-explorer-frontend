5a3693142954951b0f9dacf05b070f3da22659db
Vera Udodova <udodowa.vera@yandex.ru>
2023-06-18 18:08:23 +0300

    data, user

---
+++ src/utils/data.js
@@ -0,0 +1,44 @@
+const ownerId = 1;
+
+const movies = [
+    {
+        _id: 1,
+        nameRU: "33 слова о дизайне",
+        duration: "1ч 42м",
+        likes: [ownerId],
+        thumbnail: "./film1.png",
+        ownerId: ownerId
+    },
+    {
+        _id: 2,
+        nameRU: "Киноальманах «100 лет дизайна»",
+        duration: "1ч 25м",
+        likes: [ownerId],
+        thumbnail: "./film2.png",
+        ownerId: ownerId
+    },
+    {
+        _id: 3,
+        nameRU: "В погоне за Бенкси",
+        duration: "2ч 1м",
+        likes: [],
+        thumbnail: "./film3.png",
+        ownerId: 2
+    },
+    {
+        _id: 4,
+        nameRU: "Бег это свобода",
+        duration: "1ч 52м",
+        likes: [],
+        thumbnail: "./film4.png",
+        ownerId: 2
+    }
+];
+
+const user = {
+    _id: ownerId,
+    name: "Вера",
+    email: "email@example.com"
+}
+
+module.exports = {movies, user};
