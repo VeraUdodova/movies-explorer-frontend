9a89795f958cecf6af868e4c2cf4b5524ca96b33
Vera Udodova <udodowa.vera@yandex.ru>
2023-06-18 18:08:11 +0300

    movies card

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,13 +1,35 @@
+import { useContext} from "react";
+import {CurrentUserContext} from "../../contexts/CurrentUserContext.js";
 import "./MoviesCard.css";
 
 function MoviesCard(props) {
-    const {movie} = props;
+    const currentUser = useContext(CurrentUserContext);
+    const {movie, savedMovies} = props;
+
+    const onMovieLike = function () {
+        props.onMovieLike(movie);
+    }
+
+    const onMovieDelete = function () {
+        props.onMovieDelete(movie);
+    }
+
     return (
         <div className="moviecard">
             <div className="moviecard__info">
                 <p className="moviecard__name">{movie.nameRU}</p>
                 <p className="moviecard__duration">{movie.duration}</p>
-                <div className={`moviecard__like ${movie.like ? "moviecard__like-liked": ""}`}></div>
+                {savedMovies ?
+                    <button
+                        onClick={onMovieDelete}
+                        className={`moviecard__icon ${movie.ownerId === currentUser._id ? "moviecard__icon-delete" : ""}`}
+                    ></button>
+                    :
+                    <button
+                        onClick={onMovieLike}
+                        className={`moviecard__icon ${movie.likes.includes(currentUser._id) ? "moviecard__icon-like" : "moviecard__icon-dislike"}`}
+                    ></button>
+                }
             </div>
             <img className="moviecard__thumbnail" src={movie.thumbnail} alt={movie.nameRU}/>
         </div>
