2f43053742c6f562c6fc4ad5eed008b926709c30
Vera Udodova <udodowa.vera@yandex.ru>
2023-06-20 06:34:31 +0300

    fix

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,7 +3,6 @@ import AboutProject from "../AboutProject/AboutProject";
 import Techs from "../Techs/Techs";
 import AboutMe from "../AboutMe/AboutMe";
 import Portfolio from "../Portfolio/Portfolio";
-import "./Main.css";
 
 const Main = () => {
     const [anchor_project, anchor_techs, anchor_me] = [
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -6,12 +6,12 @@ const Promo = (props) => {
         <section className="promo">
             <div className="promo__container">
                 <h1 className="promo__title">Учебный проект студента<br/>факультета Веб-разработки.</h1>
+                <NavTab
+                    anchor_project={props.anchor_project}
+                    anchor_techs={props.anchor_techs}
+                    anchor_me={props.anchor_me}
+                />
             </div>
-            <NavTab
-                anchor_project={props.anchor_project}
-                anchor_techs={props.anchor_techs}
-                anchor_me={props.anchor_me}
-            />
         </section>
     )
 }
