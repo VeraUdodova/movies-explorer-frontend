a3edd693abbab6c20134774fc314f3663cc68209
Vera Udodova <udodowa.vera@yandex.ru>
2023-06-22 23:00:53 +0300

    menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,35 @@
-import {useLocation } from "react-router-dom";
+import {useState} from "react";
+import {useLocation} from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
 import "./Header.css";
 
 function Header(props) {
     const { pathname } = useLocation();
+    const [isBurgerMenuOpen, setBurgerMenuOpen] = useState([])
 
     if (['/signin', '/signup'].includes(pathname)) {
         return <></>;
     }
 
+    const closeBurgerMenu = function() {
+        setBurgerMenuOpen(false)
+    }
+
+    const openBurgerMenu = function() {
+        setBurgerMenuOpen(true)
+    }
+
     return (
         <header className="header">
-            <div className="header__logo"></div>
-            <Navigation loggedIn={props.loggedIn}/>
+            <div className="header__container">
+                <div className="header__logo"></div>
+                <Navigation
+                    loggedIn={props.loggedIn}
+                    isBurgerMenuOpen={isBurgerMenuOpen}
+                    closeBurgerMenu={closeBurgerMenu}
+                    openBurgerMenu={openBurgerMenu}
+                />
+            </div>
         </header>
     )
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,18 +2,53 @@ import {useLocation, Link} from "react-router-dom";
 import "./Navigation.css";
 
 function Navigation(props) {
-    const { pathname } = useLocation();
+    const {pathname} = useLocation();
+
+    const openBurgerMenu = function () {
+        props.openBurgerMenu()
+    }
+    const closeBurgerMenu = function () {
+        props.closeBurgerMenu()
+    }
 
     return (
         <>
             {props.loggedIn ?
-                <div className="navigation">
-                    <div className="navigation__menu">
-                        <Link to="/movies" className={`navigation__link ${pathname === "/movies" ? "navigation__link-selected": ""}`}>Фильмы</Link>
-                        <Link to="/saved-movies" className={`navigation__link ${pathname === "/saved-movies" ? "navigation__link-selected": ""}`}>Сохранённые фильмы</Link>
+                <>
+                    {props.isBurgerMenuOpen ? <div className="navigation__fade"></div> : <></>}
+                    <div className={`navigation ${props.isBurgerMenuOpen ? "navigation__burger" : ""}`}>
+                        {props.isBurgerMenuOpen ? <button onClick={closeBurgerMenu} className="navigation__close"></button> : <></>}
+                        <div className="navigation__menu">
+                            <Link
+                                to="/"
+                                onClick={closeBurgerMenu}
+                                className={`navigation__link navigation__link-burger ${pathname === "/" ? "navigation__link-selected" : ""}`}>
+                                Главная
+                            </Link>
+                            <Link
+                                to="/movies"
+                                onClick={closeBurgerMenu}
+                                className={`navigation__link ${pathname === "/movies" ? "navigation__link-selected" : ""}`}>
+                                Фильмы
+                            </Link>
+                            <Link
+                                to="/saved-movies"
+                                onClick={closeBurgerMenu}
+                                className={`navigation__link ${pathname === "/saved-movies" ? "navigation__link-selected" : ""}`}>
+                                Сохранённые фильмы
+                            </Link>
+                        </div>
+                        <Link
+                            to="/profile"
+                            onClick={closeBurgerMenu}
+                            className="navigation__account">
+                            Аккаунт
+                        </Link>
                     </div>
-                    <Link to="/profile" className={`navigation__account ${pathname === "/profile" ? "navigation__link-selected": ""}`}>Аккаунт</Link>
-                </div>
+                    <button
+                        onClick={openBurgerMenu}
+                        className="navigation navigation__button-burger"></button>
+                </>
                 :
                 <div className="navigation__account__menu">
                     <Link to="/signup" className="navigation__link">Регистрация</Link>
