6e124362e7d09cdbaa239d126b751e389b3255b8
Vera Udodova <udodowa.vera@yandex.ru>
2023-07-13 21:49:24 +0300

    main

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -6,11 +6,11 @@ function Movies(props) {
     return (
         <>
             <SearchForm/>
-            <MoviesCardList
-                movies={props.movies}
-                savedMovies={false}
-                onMovieLike={props.onMovieLike}
-            />
+            {/*<MoviesCardList*/}
+            {/*    movies={props.movies}*/}
+            {/*    savedMovies={false}*/}
+            {/*    onMovieLike={props.onMovieLike}*/}
+            {/*/>*/}
         </>
     )
 }
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,13 +4,17 @@ import FilterCheckbox from "../FilterCheckbox/FilterCheckbox"
 function SearchForm() {
     return (
         <form className="searchform" method="post">
-            <div className="searchform__icon"></div>
-            <input className="searchform__input" placeholder="Фильм" />
-            <button type="submit" className="searchform__button"></button>
-            <span className="searchform__line"></span>
-            <FilterCheckbox
-                title="Короткометражки"
-            />
+            <div className="searchform__block">
+                <div className="searchform__icon"></div>
+                <input className="searchform__input" placeholder="Фильм"/>
+                <button type="submit" className="searchform__button"></button>
+            </div>
+            <div className="searchform__block">
+                <span className="searchform__line"></span>
+                <FilterCheckbox
+                    title="Короткометражки"
+                />
+            </div>
         </form>
     )
 }
