11638d36a57a661c490c3e25b2ef45baaa23513d
Vera Udodova <udodowa.vera@yandex.ru>
2023-07-25 23:23:17 +0300

    fix

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -21,7 +21,6 @@ export class BaseApi {
     }
 
     _get(link) {
-        console.log(`GET ${link}`)
         this._add_token();
 
         return fetch(`${this._baseUrl}${link}`, {
@@ -31,7 +30,6 @@ export class BaseApi {
     }
 
     _save(link, method, body = []) {
-        console.log(`${method} ${link}`)
         this._add_token();
 
         return fetch(`${this._baseUrl}${link}`, {
